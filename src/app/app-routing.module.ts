9f0251fd040127f082bd372da28887863c2e8cc3
Maneesh Tewani <maneesh.tewani@gmail.com>
2018-03-20 15:59:42 -0700

    Completed workshop 204

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from "@angular/core";
+
+import { RouterModule, Routes } from '@angular/router';
+let routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule' }
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule {
+
+}
---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { VideoDashboardComponent } from './video-dashboard/video-dashboard.component';
+import { VideoListComponent } from './video-list/video-list.component';
+import { VideoPlayerComponent } from './video-player/video-player.component';
+import { StatFiltersComponent } from './stat-filters/stat-filters.component';
+import { Routes, RouterModule } from '@angular/router';
+let dashboardRoutes: Routes = [
+  { path: '', component: VideoDashboardComponent }
+];
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(dashboardRoutes)
+  ],
+  declarations: [VideoDashboardComponent, VideoListComponent, VideoPlayerComponent, StatFiltersComponent]
+})
+export class DashboardModule { }
